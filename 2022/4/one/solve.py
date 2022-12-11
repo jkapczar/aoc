26e2843da11d32ae178de0c6a46015e6bd9ff5c0
jkapczar <janos.kapczar@icloud.com>
2022-12-11 19:24:39 +0100

    day 4

---
+++ 2022/4/one/solve.py
@@ -0,0 +1,13 @@
+cnt = 0
+with open("input.txt", "r") as f:
+  for line in f.readlines():
+    line = line.strip()
+    f = line.split(",")[0]
+    s = line.split(",")[1]
+    f1 = int(f.split("-")[0])
+    f2 = int(f.split("-")[1])
+    s1 = int(s.split("-")[0])
+    s2 = int(s.split("-")[1])
+    if (f1 in range(s1, s2+1) and f2 in range(s1, s2+1)) or (s1 in range(f1, f2+1) and s2 in range(f1, f2+1)):
+      cnt += 1
+print(cnt)
