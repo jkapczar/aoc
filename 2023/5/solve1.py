9570305092431cc7a123bb1b0dccbed422260f42
jkapczar <janos.kapczar@icloud.com>
2023-12-09 11:29:10 +0100

    day5 p1

--- 2023/5/solve1.py
+++ 2023/5/solve1.py
@@ -6,6 +6,7 @@ def parse_maps(lines):
   tmp = []
   for line in lines:
     if "map" in line:
+      print(line)
       parsing = True
       continue
     if len(line) == 0:
@@ -13,11 +14,28 @@ def parse_maps(lines):
       map_table.append(tmp)
       tmp = []
       continue
-    if parsing:
-      print(line)
+    if line == lines[-1]:
       tmp.append(line.split(" "))
-  return map_table    
+      map_table.append(tmp)
+      continue
+    if parsing:
+      tmp.append(line.split(" "))
+  return map_table[1:]
 
+def mapping(s, e):
+  for m in e:
+    des = int(m[0])
+    sor = int(m[1])
+    ran = int(m[2])
+    print(f"des: {des}, sor: {sor}, ran: {ran}")
+    try:
+      v = list(range(sor, sor + ran)).index(int(s))
+      r = list(range(des, des + ran))[v]
+      print(f"found: {s}, mapped: {r}")
+      return r
+    except ValueError:
+      print(f"not found: {s}")
+  return s
 
 seeds = []
 with open(sys.argv[1], "r") as f:
@@ -27,4 +45,14 @@ with open(sys.argv[1], "r") as f:
       seeds = line.split(":")[1].strip().split(" ")
       print(seeds)
   map_table = parse_maps(lines)
-  print(map_table)
+  print(f"map_table: {map_table}")
+  res = []
+  for s in seeds:
+    r = s
+    for e in map_table:
+      print(f"seed: {r}, entry: {e}")
+      r = mapping(r, e)
+    res.append(r)
+    print(f"r: {r}")
+  print(f"res: {res}")
+  print(f"res: {min(res)}")
