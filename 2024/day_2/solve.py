be40041686c6d4749224ec30be119a5475f6d2ca
jkapczar <janos.kapczar@icloud.com>
2024-12-07 20:49:33 +0100

    day 2 was missing

--- 2024/day_2/solve.py
+++ 2024/day_2/solve.py
@@ -1,27 +1,26 @@
 res = 0
+
+def test(arr):
+  print(f"arr: {arr}, len(arr): {len(arr)}")
+  diff_arr = []
+  for i in range(0, len(arr) - 1):
+    diff = arr[i+1] - arr[i]
+    diff_arr.append(diff)
+    print(f"diff: {diff}")
+    if not (abs(diff) >= 1 and abs(diff) <= 3):
+      return False
+  print(f"diff_arr: {diff_arr}")
+  return all(x > 0 for x in diff_arr) or all(x < 0 for x in diff_arr)
+
 with open("input.txt", "r") as f:
   for line in f.readlines():
-    arr = line.strip().split(" ")
-    print(arr)
-    
-    first_diff = int(arr[1]) - int(arr[0])
-    if first_diff == 0:
-      continue
-    
-    inc = first_diff > 0
-    good = True
-    
-    for i in range(0, len(arr) - 1):
-      curr_diff = int(arr[i + 1]) - int(arr[i])
-      print(curr_diff)
-      
-      if not (abs(curr_diff) >= 1 and abs(curr_diff) <= 3 and ((curr_diff < 0 and inc == False) or (curr_diff > 0 and inc == True))):
-        good = False
-    
-    if good: 
+    arr = [int (x) for x in line.strip().split(" ")]
+    if test(arr): 
       print("good")
-      print("")
       res += 1
+    else:
+      print("bad")
+    print()  
 
 print(res)
 assert 236 == res
